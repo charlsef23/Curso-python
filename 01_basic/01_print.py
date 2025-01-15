5e0f9e7119d72820ab93ef3bf81a9efac377e408
Carlos Esteve <charlsef23@icloud.com>
2025-01-15 18:52:28 +0100

    Print

---
+++ 01_basic/01_print.py
@@ -0,0 +1,18 @@
+###
+# 01 - print()
+# El módulo print() es el módulo que nos permite imprimir en consola
+# Sirve para mostrar información en consola y te va a acompañar
+# TODA TU VIDA. Desde hoy hasta el fin de los tiempos
+###
+
+
+
+print("Hola mundo!")
+print('Esto también funciona con una comilla')
+
+print("Python", "es", "genial")
+
+print("Python", "es", "brutal", sep = "-")
+
+print("Python se imprime", end = " ")
+print("en una línea")
